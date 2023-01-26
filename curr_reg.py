530eef90e0eb17a9f1c4f9b2b5005ae6648ab7cd
Shivansh-3000 <78915727+Shivansh-3000@users.noreply.github.com>
2023-01-26 19:44:37 +0530

    Update curr_reg.py

--- curr_reg.py
+++ curr_reg.py
@@ -43,3 +43,98 @@ for i in range(1, len(currency_list)+1):
     plt.title(currency_list[i-1])
     plt.imshow(img)
 plt.show()
+
+import cv2
+Datadir = '/content/drive/MyDrive/indian_currency_new/training'
+# Datadir = 'B:/work/Data/images_original'
+category = ["10", "100", "20", "200", "2000", "50", "500", "Background"]
+dataset = []
+size1 = 256
+size2 = 256
+ct = 0
+for types in category:
+    path = os.path.join(Datadir, types)
+    for img in os.listdir(path):
+        if types == '10':
+            ex = 0
+            ct = ct + 1
+        elif types == '100':
+            ex = 1
+        elif types == '20':
+            ex = 2
+        elif types == '200':
+            ex = 3
+        elif types == '2000':
+            ex = 4
+        elif types == '50':
+            ex = 5
+        elif types == '500':
+            ex = 6
+        elif types == 'Background':
+            ex = 7
+        try:
+            img_arr = cv2.imread(os.path.join(path, img), cv2.IMREAD_GRAYSCALE)
+            new_img = cv2.resize(img_arr, (size2, size1), interpolation=cv2.INTER_AREA)
+            dataset.append([new_img, ex])
+        except Exception as e:
+            pass
+          
+X = []
+Y = []
+print("ph1 pass")
+for features, label in dataset:
+    X.append(features)
+    Y.append(label)
+X_train = np.array(X, np.float32) / 255.
+image_labels = to_categorical(Y)
+
+mean_img = X_train.mean(axis=0)
+std_dev = X_train.std(axis = 0)
+X_norm = (X_train - mean_img)/ std_dev
+X_norm, image_labels = shuffle(X_norm, image_labels, random_state=0)
+
+Xtrain, Xvalid, Ytrain, Yvalid = train_test_split(X_norm, image_labels, test_size=0.2, random_state=7)
+import tensorflow as tf
+from sklearn.metrics import accuracy_score
+from tensorflow import keras
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense, Conv2D, MaxPool2D, Flatten, BatchNormalization, Dropout,MaxPooling2D
+
+model = Sequential()
+model.add(BatchNormalization(input_shape=Xtrain.shape[1:]))
+model.add(Conv2D(32, kernel_size=(3, 3), activation= 'relu',padding= 'same'))
+model.add(Conv2D(32, kernel_size=(3, 3), activation= 'relu'))
+model.add(MaxPooling2D(pool_size=(3,3)))
+model.add(Dropout(0.4))
+
+model.add(Conv2D(64, kernel_size=(3, 3), activation= 'relu', padding='same'))
+model.add(Conv2D(64, kernel_size=(3, 3), activation= 'relu'))
+model.add(MaxPooling2D(pool_size=(3,3)))
+model.add(Dropout(0.3))
+
+model.add(Conv2D(128, kernel_size=(3, 3), activation= 'relu', padding='same'))
+model.add(Conv2D(128, kernel_size=(3, 3), activation= 'relu'))
+model.add(MaxPooling2D(pool_size=(3,3)))
+model.add(Dropout(0.3))
+
+model.add(Conv2D(256, kernel_size=(3, 3), activation= 'relu', padding='same'))
+model.add(Conv2D(256, kernel_size=(3, 3), activation= 'relu'))
+model.add(MaxPooling2D(pool_size=(3,3)))
+model.add(Dropout(0.3))
+
+model.add(Flatten())
+model.add(Dropout(0.5))
+model.add(Dense(8, activation='softmax'))
+
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.summary()
+model.get_config()
+model.layers[0].get_config()
+model.layers[0].input_shape			
+model.layers[0].output_shape			
+model.layers[0].get_weights()
+np.shape(model.layers[0].get_weights()[0])
+model.layers[0].trainable
+early_stops = EarlyStopping(patience=3, monitor='val_acc')
+
+model.save('model1_final_B8.h5')
